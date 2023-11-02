7705536bbeab2ea3ee1b8b0ee6e3add19c55491c
derjo <jonas@SH-Jonas-Mager.localdomain>
2023-11-02 20:06:11 +0100

    config Datei strukturiert

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 from docx.shared import Pt
 
+# program
 TOMEDO_CACHE_PROXY = str(Path.home()) + "/.tomedoCache/temporaryFiles/proxy/"
 SQLITE_FILE_NAME = str(Path(__file__).parent.resolve()) + "/sqlite.db"
+# document
 NORMAL_FONT_NAME = "Nexa"
 NORMAL_FONT_SIZE = Pt(10)
 NORMAL_PARAGRAPH_SPACE_BEFORE = Pt(0)
