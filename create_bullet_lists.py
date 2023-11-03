b41a4ade39e6681fb0620e5fca41300b3b6429c3
derjo <jonas@SH-Jonas-Mager.localdomain>
2023-11-03 19:32:24 +0100

    some minor config changed

--- create_bullet_lists.py
+++ create_bullet_lists.py
@@ -2,21 +2,37 @@ import re
 import docx_utils
 import config
 
+# def add_paragraph_after_list(p):
+#     if re.match(r"(\*\*(.*)\n?)+", p.text):
+#         docx_utils.insert_paragraph_after(p, "")
+
 def insert_bullet_list(p):
     i = 0
     while i < len(p.text.splitlines()):
         line = p.text.splitlines()[i]
         if re.match(config.RE_BULLETPOINT_LIST_ITEM, line):
             value = re.split(config.RE_BULLETPOINT_LIST_ITEM, line)[2]
-            docx_utils.insert_paragraph_after(p, docx_utils.concate(p.text.splitlines()[i+1:]))
+            if (
+                docx_utils.concate(p.text.splitlines()[i + 1 :]) != ""
+                or config.LEAVE_EMPTY_PARAGRAPH_AFTER_LIST
+            ):
+                docx_utils.insert_paragraph_after(
+                    p, docx_utils.concate(p.text.splitlines()[i + 1 :])
+                )
             p.text = docx_utils.concate(p.text.splitlines()[:i])
-            docx_utils.insert_paragraph_after(p, value, 'List Bullet')
-        i += 1
-        if p.text == '':
+            docx_utils.insert_paragraph_after(p, value, "List Bullet")
+        if p.text == "":
             docx_utils.delete_paragraph(p)
             break
+        i += 1
 
 def run(document):
+    # j = 0
+    # while j < len(document.paragraphs):
+    #     p = document.paragraphs[j]
+    #     add_paragraph_after_list(p)
+    #     j += 1
+
     j = 0
     while j < len(document.paragraphs):
         p = document.paragraphs[j]
