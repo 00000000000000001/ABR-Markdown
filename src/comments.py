a30632e08620030cf7734b87fa5afcf6b86bade8
derjo <jonas@SH-Jonas-Mager.local>
2024-02-22 12:55:48 +0100

    refactor comments

--- src/comments.py
+++ src/comments.py
@@ -4,7 +4,6 @@ from gui import showMsg
 
 def removeComments(doc):
     wasEdited = False
-    consume = False
 
     for p in doc.paragraphs:
         text = p.text
@@ -21,8 +20,8 @@ def removeComments(doc):
             if text[i] == "{":
                 j = text.find("}", i)
                 if not j == -1:
-                    wasEdited = True
                     text = rm(i, j, p).text
+                    wasEdited = True
                     if i < len(text) and (
                         i == 0 or (text[i] == "\n" and text[i - 1] == "\n")
                     ):
@@ -30,10 +29,6 @@ def removeComments(doc):
                             text = rm(i, i, p).text
                 else:
                     break
-            elif text[i] == "\n" and consume:
-                text = rm(i, i, p).text
-            else:
-                consume = False
                 i = text.find("{", i)
 
     return wasEdited
