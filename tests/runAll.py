40663b7e104cce54952843ac45f0e1ebf027973f
derjo <jonas@SH-Jonas-Mager.local>
2024-02-22 09:59:58 +0100

    updatescript added

--- tests/runAll.py
+++
@@ -1,7 +0,0 @@
-import tests.test_bulletList as test_bulletList, tests.test_docxTools as test_docxTools, tests.test_wasEdited as test_wasEdited, tests.test_comments as test_comments, tests.test_paragraph as test_paragraph
-
-test_bulletList.test_replaceDoubleAsterisks()
-test_docxTools.test_docxTools()
-test_wasEdited.test_wasEdited()
-test_comments.test_removeComments()
-test_paragraph.test_insertParagraphs()
